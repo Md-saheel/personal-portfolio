8934aeacce5b1648b9f0471063af95a852561e4c
Mohamed Saheel <110540324+Md-saheel@users.noreply.github.com>
2024-07-30 13:56:34 +0530

    responsive

--- data/projects.js
+++ data/projects.js
@@ -17,7 +17,7 @@ export const projects = [
   {
     name: 'Pricing Table Project',
     image: 'img/pro8.png',
-    link: 'https://md-saheel.github.io/Pricing-Table-Project/">',
+    link: 'https://md-saheel.github.io/Pricing-Table-Project/',
   },
   {
     name: 'Landing Page',
